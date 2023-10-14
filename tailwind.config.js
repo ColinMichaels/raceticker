a6f637685ddbd162d64a1dfc322c0d7fe2eeb4d9
Colin Michaels <colin@colinmichaels.com>
2023-10-14 17:58:17 -0400

    Resizing

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    fontSize: {
+      sm: '0.6rem',
+      base: '0.8rem',
+      '2xl': '1.0rem'
+    },
     extend: {
       fontFamily: {
         'main-font' : ['Montserrat', 'sans-serif']
